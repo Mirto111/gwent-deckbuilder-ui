a090cb6205818f25b3be49aaa1d9243090dba50d
Mirto111 <mitritdil@ya.ru>
2019-09-12 09:04:50 +0300

    fix app.css select faction, delete default files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "./styles/App.css";
-import "./styles/cards-style.css";
+import React, { Component } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
+import './styles/cards-style.css';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-import Header from "./ui/Header";
-import DeckBuilder from "./ui/DeckBuilder";
-import DeckLayout from "./layout/DeckLayout";
-import DeckBuilderLayout from "./layout/DeckBuilderLayout";
-import LeadersLayout from "./layout/LeadersLayout";
-import FilterGroup from "./ui/FilterGroup";
+import Header from './ui/Header';
+import DeckBuilder from './ui/DeckBuilder';
+import DeckLayout from './layout/DeckLayout';
+import DeckBuilderLayout from './layout/DeckBuilderLayout';
+import LeadersLayout from './layout/LeadersLayout';
+import FilterGroup from './ui/FilterGroup';
 import store from './store';
-import { ADD_LEADERS_CARDS, ADD_CARDS, ADD_FILTERED_CARDS, GET_FILTERS_LEADERS } from './constants/actionTypes';
+import {
+ ADD_LEADERS_CARDS, ADD_CARDS, ADD_FILTERED_CARDS, GET_FILTERS_LEADERS 
+} from './constants/actionTypes';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     filtered:[],
+     filtered: [],
     };
-
-
   }
 
   partitionContent(r) {
-    let leaders = [];
-    let cards = [];
+    const leaders = [];
+    const cards = [];
     const empty = [];
 
-    for (var key = 0; key < r.length; key++) {
-      if (r[key]["cardTier"] === "Leader") {
+    for (let key = 0; key < r.length; key++) {
+      if (r[key].cardTier === 'Leader') {
         leaders.push(r[key]);
       } else {
         cards.push(r[key]);
       }
     }
 
-    cards.sort(function(a, b) {
+    cards.sort((a, b) => {
         const cardTiers = {"Gold":1, "Bronze":2};
 
       return (b.provision - a.provision + cardTiers[a.cardTier] - cardTiers[b.cardTier]);
@@ -44,37 +44,33 @@ class App extends Component {
 
     store.dispatch({ type: ADD_LEADERS_CARDS, leaders });
     store.dispatch({ type: ADD_CARDS, cards });
-    //store.dispatch({ type: ADD_FILTERED_CARDS, cards });
-    //store.dispatch({ type: GET_FILTERS_LEADERS, empty});
+    // store.dispatch({ type: ADD_FILTERED_CARDS, cards });
+    // store.dispatch({ type: GET_FILTERS_LEADERS, empty});
 
-    //this.setState({filtered: others});
+    // this.setState({filtered: others});
   }
 
   componentDidMount() {
     axios
-      .get("http://localhost:8080/api/cards")
-      .then(response => this.partitionContent(response.data));
+      .get('http://localhost:8080/api/cards')
+      .then((response) => this.partitionContent(response.data));
   }
 
 
-
   render() {
-    const {filtered} = this.state;
+    const { filtered } = this.state;
 
     return (
       <Router>
-      <Header/>
-      <FilterGroup
-      />
-      <Route path='/decks' component={DeckLayout} />
-      <Route path='/deckbuilder' component={DeckBuilder} />
+        <Header />
+        <FilterGroup />
+        <Route path="/decks" component={DeckLayout} />
+        <Route path="/deckbuilder" component={DeckBuilder} />
       </Router>
 
     );
   }
 }
-
-
 
 
 export default App;
